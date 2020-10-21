9db012bd5113b952d3a322129e3354e71c62bb53
Michael Chau <michael@designquest.com.hk>
2020-10-22 00:48:39 +0800

    handle short dist

--- routes/api.php
+++ routes/api.php
@@ -30,28 +30,35 @@ Route::get('/rtl-altitude/{path}', function ($path) {
     $lon1 = explode(",",$locations[0])[1];
     $lat2 = explode(",",$locations[1])[0];
     $lon2 = explode(",",$locations[1])[1];
-    if (($lat1 == $lat2) && ($lon1 == $lon2)) {
-        $dist = 0;
+    $theta = $lon1 - $lon2;
+    $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+    $dist = acos($dist);
+    $dist = rad2deg($dist);
+    $dist = $dist * 60 * 1.1515 * 1.609344 *1000; // Metre
+    if ($dist < 20) {
+        $url = $api."?key=".$apikey."&locations=".$locations[1];
+        $response = file_get_contents($url);
+        $response = json_decode($response);
+        $result["max_alt"] = $response->results[0]->elevation;
+        $result["total_dist"] = $dist;
+        $result["sample_dist"] = 0;
     } else {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $dist = $dist * 60 * 1.1515 * 1.609344 *1000; // Metre
-    }
-    $samples = ceil($dist/20);
-    if ($samples > 500) {
-        $samples = 500; // The max Google accepts samples
-    }
-    $url = $api."?key=".$apikey."&path=".$path."&samples=".$samples;
-    $response = file_get_contents($url);
-    $response = json_decode($response);
-    $max = max(array_map(function($o) {
-        return $o->elevation;
-    }, $response->results));
-    $result["max_alt"] = $max;
-    $result["total_dist"] = $dist;
-    $result["sample_dist"] = $dist / $samples;
+        $samples = ceil($dist/20);
+        if ($samples > 500) {
+            $samples = 500; // The max Google accepts samples
+        }
+        $url = $api."?key=".$apikey."&path=".$path."&samples=".$samples;
+        $response = file_get_contents($url);
+        $response = json_decode($response);
+        $max = max(array_map(function($o) {
+            return $o->elevation;
+        }, $response->results));
+        $result["max_alt"] = $max;
+        $result["total_dist"] = $dist;
+        $result["sample_dist"] = $dist / $samples;
+    }    
 
     return response()->json($result);
 });
+
+
