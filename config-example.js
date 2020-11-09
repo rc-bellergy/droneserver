0349a92fb08ab0e56fd4ccb3dd26756740e8d49d
Bellergy RC <rc@bellergy.com>
2020-11-09 17:39:32 +0800

    update

--- config-example.js
+++ config-example.js
@@ -1,3 +1,6 @@
-module.exports = {
-    GOOGLE_API_KEY: "AIza-your-api-key" /* change it to your API key */
+const config = {
+    GOOGLE_API_KEY: "AIza-your-api-key" /* change it to your API key */,
+    LOG_FILE_PATH: "logs/"
 }
+
+export { config };
