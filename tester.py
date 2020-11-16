98eef8f92d5e63fb5a0d1716bc40ddb79f3e9931
Bellergy RC <rc@bellergy.com>
2020-11-17 00:21:06 +0800

    Add random move

--- tester.py
+++ tester.py
@@ -4,8 +4,8 @@ import asyncio
 import socketio
 import random
 
-
 droneserver = 'http://droneserver.zt:3000?user=Dummy-Drone'
+
 
 async def run():
     print("Connecting to droneserver")
@@ -18,13 +18,18 @@ async def run():
     await sio.emit('home_location_updated', location)
 
     drone_location = { "lat": 22.309058, "lon": 114.304060, "alt": 50, "heading": 270 }
+    lat_move = 0.0
+    lon_move = 0.0
+    
     while True:
         # Update drone location
         # location = { "lat": 22.307990, "lon": 114.301596 }
         await sio.emit('drone_location_updated', drone_location)
         
-        drone_location['lat'] = drone_location['lat'] - 0.00005
-        drone_location['lon'] = drone_location['lon'] - 0.00005
+        lat_move = lat_move + random.uniform(-0.00005,0.00005)
+        lon_move = lon_move + random.uniform(-0.00005,0.00005)
+        drone_location['lat'] = drone_location['lat'] + lat_move
+        drone_location['lon'] = drone_location['lon'] + lon_move
         drone_location['heading'] = drone_location['heading'] + 25
 
         await asyncio.sleep(2)
