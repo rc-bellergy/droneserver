8f0e4218c2c1627b09aac6a2fe5a44e81bef80ba
Bellergy RC <rc@bellergy.com>
2020-12-10 09:15:18 +0800

    Add Mabbox API key template

--- config-example.js
+++ config-example.js
@@ -1,5 +1,12 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const config = {
-    GOOGLE_API_KEY: "AIza-your-api-key" /* change it to your API key */,
+    GOOGLE_API_KEY: "AIza-your-api-key" /* change it to your Google Map API key */,
+    MAPBOX_API_KEY: "pk.eyJ1-your-api-key" /* change it to your MAPBOX API key */,
     LOG_FILE_PATH: "logs/"
 }
 
