176502e08ef6758f022ffc543472e164cb291c8f
Bellergy RC <rc@bellergy.com>
2020-12-15 23:27:03 +0800

    Testing get building height from Mapbox API

--- mapbox-api-tseting.js
+++ mapbox-api-tseting.js
@@ -1,26 +1,36 @@
 /*
-Testing multiple http requests to mapbox api
+Testing:
+1. multiple http requests to mapbox api
+2. get buildings features
+
 */
 
 import { config } from './config.js';
+import { Client } from '@googlemaps/google-maps-services-js';
 import request from "request-promise";
 
 const gateway="https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/";
 const key="access_token=pk.eyJ1IjoiZHFtaWNoYWVsIiwiYSI6ImNrZ2hodGlxMjBiZmszMHBnYmlrdGU4OTEifQ.g_yEmozIITINLVoEtDXCag";
-const locations = ["114.23623241544855,22.307360431666915", "114.30739452086544,22.30423718305112"];
-const urls = [
-    gateway + locations[0] + ".json?radius=10&layers=building&" + key,
-    gateway + locations[1] + ".json?radius=10&layers=building&" + key,
+const locations = [
+    "114.23623241544855,22.307360431666915", 
+    "114.30739452086544,22.30423718305112",
+    "114.26991417887967,22.296903502407137",
+    "114.21944338507242,22.287641500421007",
+    "114.21335521273765,22.287574395080807"
 ];
+const promises = locations.map(location => request(gateway + location + ".json?radius=10&layers=building&access_token=" + config.MAPBOX_API_KEY));
 
-const promises = urls.map(url => request(url));
+var buildingHeight = [];
+
 Promise.all(promises).then((data) => {
-    console.log(data);
-    
-    /* It will return
-    [
-        '{"type":"FeatureCollection","features":[{"type":"Feature","id":121701404,"geometry":{"type":"Point","coordinates":[114.23623241544855,22.307360431666915]},"properties":{"extrude":"true","iso_3166_1":"HK","underground":"false","height":117,"type":"building","min_height":0,"iso_3166_2":"CN-91","tilequery":{"distance":0,"geometry":"polygon","layer":"building"}}}]}',
-        '{"type":"FeatureCollection","features":[]}'
-    ] */
-    
+
+    data.forEach(location => {
+        var features = JSON.parse(location).features;
+        var height = 0;
+        features.forEach(feature => {
+            height = height < feature.properties.height ? feature.properties.height : height;
+        });
+        buildingHeight.push(height);
+    })
+    console.log(buildingHeight);
 });
